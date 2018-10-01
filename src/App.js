18fdb1f4d9dd60d47feb11a2d171bddf2923e62f
Laszlo Bedekovics <bedeko@laszlos-mbp.home>
2018-10-01 23:29:18 +0100

    8

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
-//import Data from './components/data';
-//import Input from './components/inputfield';
-import Input from './components/input';
+import React, { Component } from "react";
+import Data from "./components/data";
+import Navbar from "./components/navbar";
+//import Input from './components/input';
 //import Gridcss from './components/cssgrid';
 //import Button from './components/button';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      
       <div className="App">
-       <Input  />
-       
-       
+        <Navbar />
+        <Data />
       </div>
     );
   }
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,112 +1,148 @@
 import React, { Component } from "react";
 import fire from "../fbase";
-//import key from 'weak-key';
 import styles from "./styles.css";
-import Input from "./inputfield";
 
 class Data extends Component {
-  //state = { closing: [], items: ['J2o', 'Coke', 'Orange', 'Apple','Mango', 'Pepsi'] }
-  state = { items: [], opening: [], sale: [], closing: [] };
-  //constructor(props) {
-  // super(props);
-  // function handleinputs (){
+  state = {
+    items: [],
+    opening: [],
+    sale: [],
+    closing: [],
+    rid: [],
+    value: ""
+  };
 
-  //     return (var len = this.state.items.opening.length;)
-  // console.log(len);
-  // }
-  
   componentWillMount() {
     let itemRef = fire.database().ref("list/items");
     itemRef.on("value", snapshot => {
       let items = { id: snapshot.key, text: snapshot.val() };
       let itemnames = items.text;
-      //let itemid = items.id;
       this.setState({ items: itemnames });
-      //console.log(itemid);
     });
     let openRef = fire.database().ref("list/opening");
     openRef.on("value", snapshot => {
       let opening = { id: snapshot.key, text: snapshot.val() };
       let openingqty = opening.text;
       this.setState({ opening: openingqty });
-      console.log(openingqty);
     });
     let saleRef = fire.database().ref("list/sale");
     saleRef.on("value", snapshot => {
       let sale = { id: snapshot.key, text: snapshot.val() };
       let saleqty = sale.text;
       this.setState({ sale: saleqty });
-      console.log(saleqty);
     });
     let closingRef = fire.database().ref("list/closing");
     closingRef.on("value", snapshot => {
       let closing = { id: snapshot.key, text: snapshot.val() };
       let closingqty = closing.text;
       this.setState({ closing: closingqty });
-      console.log(closingqty);
+    });
+    let ridRef = fire.database().ref("list/rid");
+    ridRef.on("value", snapshot => {
+      let rid = { id: snapshot.key, text: snapshot.val() };
+      let ridqty = rid.text;
+      this.setState({ rid: ridqty });
     });
   }
+  handleChange = e => {
+    const value = { ...this.state.value };
+    value[e.currentTarget.id] = e.currentTarget.value;
+    this.setState({ value });
+  };
+  addMessage = e => {
+    e.preventDefault();
+    let value = this.state.value;
+    fire
+      .database()
+      .ref("list/closing/")
+
+      .set(value);
+    this.cancelCourse();
+  };
+  cancelCourse = () => {
+    document.getElementById("myform").reset();
+  };
 
   render() {
     return (
-      <div styles={styles.wrapper} className="wrapper">
-        <div styles={styles.header1} className="header1">
-          Items
-        </div>
-        <div styles={styles.header2} className="header2">
-          {" "}
-          Opening Stock
-        </div>
-        <div styles={styles.header3} className="header3">
-          Sale
-        </div>
-        <div styles={styles.header4} className="header4">
-          Closing Stock
-        </div>
-        <div styles={styles.header5} className="header5">
-          Closing Stock
-        </div>
+      <div>
+        <div styles={styles.wrapper} className="wrapper">
+          <div styles={styles.header1} className="header1">
+            Items
+          </div>
+          <div styles={styles.header2} className="header2">
+            Opening
+          </div>
+          <div styles={styles.header3} className="header3">
+            Sale
+          </div>
+          <div styles={styles.header4} className="header4">
+            Closing
+          </div>
+          <div styles={styles.header5} className="header5">
+            Update Closing
+          </div>
 
-        <div styles={styles.items} className="items">
-          {this.state.items.map(i => (
-            <p styles={styles.rowstyle} className="rowstyle" key={i.id}>
-              {i}
-            </p>
-          ))}
-        </div>
-        <div styles={styles.opening} className="opening">
-          {this.state.opening.map(o => (
-            <p styles={styles.rowstyle} className="rowstyle" key={o.id}>
-              {o}
-            </p>
-          ))}
-        </div>
+          <div styles={styles.items} className="items">
+            {this.state.items.map(i => (
+              <p styles={styles.rowstyle} className="rowstyle" key={i.id}>
+                {i}
+              </p>
+            ))}
+          </div>
+          <div styles={styles.opening} className="opening">
+            {this.state.opening.map(o => (
+              <p styles={styles.rowstyle} className="rowstyle" key={o.id}>
+                {o}
+              </p>
+            ))}
+          </div>
 
-        <div styles={styles.sale} className="sale">
-          {this.state.sale.map(s => (
-            <p styles={styles.rowstyle} className="rowstyle" key={s.id}>
-              {s}
-            </p>
-          ))}
-        </div>
+          <div styles={styles.sale} className="sale">
+            {this.state.sale.map(s => (
+              <p styles={styles.rowstyle} className="rowstyle" key={s.id}>
+                {s}
+              </p>
+            ))}
+          </div>
 
-        <div styles={styles.closing} className="closing">
-          {this.state.closing.map(c => (
-            <p styles={styles.rowstyle} className="rowstyle" key={c.id}>
-              {c}
-            </p>
-          ))}
-        </div>
-
-        <div styles={styles.inpute} className="inpute">
-          {this.state.items.map(i => (
-            <Input id={i} />
-          ))}
+          <div styles={styles.closing} className="closing">
+            {this.state.closing.map(c => (
+              <p styles={styles.rowstyle} className="rowstyle" key={c.id}>
+                {c}
+              </p>
+            ))}
+          </div>
+          <div>
+            <div>
+              <form id="myform" styles={styles.inpute} className="inpute">
+                {this.state.rid.map(i => (
+                  <input
+                    onChange={this.handleChange}
+                    className="form-control"
+                    type="text"
+                    placeholder="Closing"
+                    id={i}
+                    key={i}
+                    name="myForm"
+                  />
+                ))}
+              </form>
+            </div>
+          </div>
+          <div>
+            <button
+              className="btn btn-success"
+              // styles={styles.rowstyle}
+              // className="rowstyle"
+              onClick={this.addMessage}
+            >
+              Submit
+            </button>
+          </div>
         </div>
       </div>
     );
   }
- 
 }
-
 export default Data;
